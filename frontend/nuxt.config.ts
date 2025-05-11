f3a4ff3f5e1270e8473201e632d97f53eca32d58
programadortigre <programadortigre@gmail.com>
2025-05-11 17:39:18 -0500

    cambie todo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
-  ssr: true
+  ssr: true, // Habilita el rendering del lado del servidor (SSR)
+  build: {
+    transpile: ['@nuxtjs/axios'], // Si usas axios o librerías adicionales
+  },
+  axios: {
+    baseURL: 'http://localhost:8000', // URL de la API FastAPI
+  }
 })
