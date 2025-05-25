b5efdc194b4293fd77fe2189acd7a1ac42f0c434
programadortigre <programadortigre@gmail.com>
2025-05-25 11:21:19 -0500

    modelos activados

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,11 +1,11 @@
-from .modelos import *
-# from .personas import *
-# from .roles import *
-# from .usuarios import *
-# from .permisos import *
-# from .roles_permisos import *
-# from .logs_usuarios import *
-# from .tokens import *
-# from .tipos_perfil import *
-# from .perfiles import *
-# from .configuraciones import *
+# from .modelos import *
+from .personas import *
+from .roles import *
+from .usuarios import *
+from .permisos import *
+from .roles_permisos import *
+from .logs_usuarios import *
+from .tokens import *
+from .tipos_perfil import *
+from .perfiles import *
+from .configuraciones import *
